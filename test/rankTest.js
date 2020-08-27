6c18378f5dcdb43576d9deb6fda579a9e0168d94
howevert <1413537501@qq.com>
2020-08-27 21:41:29 +0800

    [Refactor Homework][David] add test Case 2,should return A when given voyage with china 12 and history length 5

--- test/rankTest.js
+++ test/rankTest.js
@@ -19,3 +19,75 @@ rankTest('Case 1,should return B given length 20 and history 1', t => {
   t.is('B', result);
 
 });
+
+rankTest('Case 2,should return A when given voyage with china 12 and history length 5 ', t => {
+  //given
+  const voyage = {
+    zone: 'china',
+    length: 12,
+  };
+  const history = [
+    {
+      zone: 'china',
+      profit: 5,
+    },
+    {
+      zone: 'A',
+      profit: 7,
+    },
+    {
+      zone: 'B',
+      profit: 4,
+    },
+    {
+      zone: 'C',
+      profit: 3,
+    },
+    {
+      zone: 'D',
+      profit: 2,
+    },{
+      zone: 'E',
+      profit: 7,
+    },
+    {
+      zone: 'F',
+      profit: 8,
+    },
+    {
+      zone: 'G',
+      profit: 9,
+    },
+    {
+      zone: 'H',
+      profit: 2,
+    },
+    {
+      zone: 'I',
+      profit: 1,
+    },{
+      zone: 'J',
+      profit: 1,
+    },
+    {
+      zone: 'K',
+      profit: 4,
+    },
+    {
+      zone: 'L',
+      profit: 1,
+    },
+    {
+      zone: 'M',
+      profit: 1,
+    },
+    {
+      zone: 'N',
+      profit: 1,
+    }
+  ];
+  //when
+  let result = rating(voyage, history);
+  //then
+  t.is('A', result);
+})
