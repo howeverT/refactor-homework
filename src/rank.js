b76ba66371876ab905f1f129de2acb7cace669ee
howevert <1413537501@qq.com>
2020-08-28 08:36:01 +0800

    [Refactor Homework][David] refactor: use strategies to replace if branch

--- src/rank.js
+++ src/rank.js
@@ -1,4 +1,4 @@
-function voyageRisk (voyage) {
+function voyageRisk(voyage) {
   let result = 1;
   if (voyage.length > 4) {
     result += 2;
@@ -19,11 +19,11 @@ function isIncludeMainland(voyage) {
   ].includes(voyage.zone);
 }
 
-function hasChina (history) {
+function hasChina(history) {
   return history.some(v => 'china' === v.zone);
 }
 
-function captainHistoryRisk (voyage, history) {
+function captainHistoryRisk(voyage, history) {
   let result = 1;
   if (history.length < 5) {
     result += 4;
@@ -39,35 +39,47 @@ function isChina(voyage) {
   return voyage.zone === 'china';
 }
 
-function voyageProfitFactor (voyage, history) {
+function voyageProfitFactor(voyage, history) {
   let result = 2;
-  if (voyage.zone === 'china'||voyage.zone === 'east-indies') {
+  if (voyage.zone === 'china' || voyage.zone === 'east-indies') {
     result += 1;
   }
   if (voyage.zone === 'china' && hasChina(history)) {
     result += 3;
-    if (history.length > 10) {
-      result += 1;
-    }
-    if (voyage.length > 12) {
-      result += 1;
-    }
-    if (voyage.length > 18) {
-      result -= 1;
-    }
+    result = calculateLength(10, history.length, result)
+    result = calculateLength(12, voyage.length, result)
+    result = calculateLength(18, voyage.length, result)
   }
   else {
-    if (history.length > 8) {
-      result += 1;
-    }
-    if (voyage.length > 14) {
-      result -= 1;
-    }
+    result = calculateLength(8, history.length, result)
+    result = calculateLength(14, voyage.length, result)
   }
   return result;
 }
 
-function rating (voyage, history) {
+var strategies = {
+  10: function (result, length) {
+    return length > 10 ? result += 1 : result;
+  },
+  12: function (result, length) {
+    return length > 12 ? result += 1 : result;
+  },
+  18: function (result, length) {
+    return length > 18 ? result -= 1 : result;
+  },
+  8: function (result, length) {
+    return length > 8 ? result += 1 : result;
+  },
+  14: function (result, length) {
+    return length > 14 ? result -= 1 : result;
+  }
+};
+
+var calculateLength = function (strategy, length, result) {
+  return strategies[strategy](result, length);
+};
+
+function rating(voyage, history) {
   const vpf = voyageProfitFactor(voyage, history);
   const vr = voyageRisk(voyage);
   const chr = captainHistoryRisk(voyage, history);
@@ -91,10 +103,10 @@ const history = [
   {
     zone: 'east-indies',
     profit: 5,
-  },{
+  }, {
     zone: 'west-indies',
     profit: 15,
-  },{
+  }, {
     zone: 'china',
     profit: -2,
   },
