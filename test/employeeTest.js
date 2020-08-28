d5b0f36bcac5a2871a2439832e5140045c708226
howevert <1413537501@qq.com>
2020-08-28 11:40:55 +0800

    [Refactor Homework][David] refactor: add test Case 2,type is manager

--- test/employeeTest.js
+++ test/employeeTest.js
@@ -9,3 +9,12 @@ employeeTest('Case 1,type is engineer', t => {
     //then
     t.is('Ben (engineer)', result);
 });
+
+employeeTest('Case 2,type is manager', t => {
+    //given
+    let employee = new Employee('Ben', 'manager')
+    //when
+    let result = employee.toString();
+    //then
+    t.is('Ben (manager)', result);
+});
