1b915377240237f44508d3a3d3fbce8ffe8c1f04
howevert <1413537501@qq.com>
2020-08-28 18:19:37 +0800

    [Refactor Homework][David] add test Case 2,should return 3 given isRush true and deliveryState NY

--- test/deliveryTest.js
+++ test/deliveryTest.js
@@ -14,3 +14,17 @@ deliveryTest('Case 1,should return 2 given isRush true and deliveryState MA', t 
     const result = deliveryDate(anorder, isRush);
     t.is(result, 2);
 });
+
+deliveryTest('Case 2,should return 3 given isRush true and deliveryState NY', t => {
+    const isRush = true;
+    const anorder = {
+        deliveryState: 'NY',
+        placedOn:{
+            plusDays:(time)=>{
+                return time;
+            }
+        }
+    }
+    const result = deliveryDate(anorder, isRush);
+    t.is(result, 3);
+});
